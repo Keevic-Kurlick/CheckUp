9abcb5404af1954f838befd8f388064b109ef62a
Weipy <nikita.savelev1999@mail.ru>
2021-12-25 15:32:55 +0300

    Output of services from the database.

--- resources/views/layouts/menu/services.blade.php
+++ resources/views/layouts/menu/services.blade.php
@@ -1,3 +1,12 @@
+@php
+
+/**
+* @var \App\Models\Service[] $services
+*/
+@endphp
+
+
+
 @extends('layouts.app')
 
 @section('css')
@@ -7,50 +16,21 @@
 @section('content')
     <div class = "container" style="margin-bottom: 180px; align-content:center;justify-content: space-between">
         <div class = "row">
-            <div class = "col-xl-3 col-lg-4 col-md-6 col-s-12">
-                <div class="card" style="width: 18rem; margin:10px">
-                    <img class="card-img-top-green" src="{{asset('storage/logos/notes-svgrepo-com.svg')}}" alt="Справка">
-                    <div class="card-body">
-                        <h5 class="card-title" style="font-weight: bold; text-align: center">Справка 003/в-у</h5>
-                        <p class="card-text">Справка для получения водительского удостоверения</p>
-                        <p class="card-text">1500 руб.</p>
-                        <a href="#" class="btn btn-primary" style="display:flex; justify-content: center;background-color:#00C98D; color:white; font-weight: bold">Заказать</a>
+            @foreach($services as $service)
+                <div class="col-xl-3 col-lg-4 col-md-6 col-s-12">
+                    <div class="card" style="width: 18rem; margin:10px">
+                        <img class="card-img-top-green" src="{{asset('storage/logos/notes-svgrepo-com.svg')}}"
+                             alt="Справка">
+                        <div class="card-body">
+                            <h5 class="card-title"
+                                style="font-weight: bold; text-align: center">{{ $service->name }}</h5>
+                            <p class="card-text">{{ $service->price }} руб.</p>
+                            <a href="#" class="btn btn-primary"
+                               style="display:flex; justify-content: center;background-color:#00C98D; color:white; font-weight: bold">Заказать</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class = "col-xl-3 col-lg-4 col-md-6 col-s-12">
-                <div class="card" style="width: 18rem; margin:10px">
-                    <img class="card-img-top-green" src="{{asset('storage/logos/notes-svgrepo-com.svg')}}" alt="Справка">
-                    <div class="card-body">
-                        <h5 class="card-title" style="font-weight: bold; text-align: center">Справка 003/в-у</h5>
-                        <p class="card-text">Справка для получения водительского удостоверения</p>
-                        <p class="card-text">1500 руб.</p>
-                        <a href="#" class="btn btn-primary" style="display:flex; justify-content: center;background-color:#00C98D; color:white; font-weight: bold">Заказать</a>
-                    </div>
-                </div>
-            </div>
-            <div class = "col-xl-3 col-lg-4 col-md-6 col-s-12">
-                <div class="card" style="width: 18rem; margin:10px">
-                    <img class="card-img-top-green" src="{{asset('storage/logos/notes-svgrepo-com.svg')}}" alt="Справка">
-                    <div class="card-body">
-                        <h5 class="card-title" style="font-weight: bold; text-align: center">Справка 003/в-у</h5>
-                        <p class="card-text">Справка для получения водительского удостоверения</p>
-                        <p class="card-text">1500 руб.</p>
-                        <a href="#" class="btn btn-primary"style="display:flex; justify-content: center;background-color:#00C98D; color:white; font-weight: bold">Заказать</a>
-                    </div>
-                </div>
-            </div>
-            <div class = "col-xl-3 col-lg-4 col-md-6 col-s-12">
-                <div class="card" style="width: 18rem; margin:10px">
-                    <img class="card-img-top-green" src="{{asset('storage/logos/notes-svgrepo-com.svg')}}" alt="Справка">
-                    <div class="card-body">
-                        <h5 class="card-title" style="font-weight: bold; text-align: center">Справка 003/в-у</h5>
-                        <p class="card-text">Справка для получения водительского удостоверения</p>
-                        <p class="card-text">1500 руб.</p>
-                        <a href="#" class="btn btn-primary"style="display:flex; justify-content: center;background-color:#00C98D; color:white; font-weight: bold">Заказать</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
