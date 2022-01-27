370456899752a894ae11363725c3252d3ed6dc7e
Weipy <nikita.savelev1999@mail.ru>
2022-01-27 22:04:39 +0300

    add_user_roles: added user roles.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,12 +2,40 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @method static whereName(string $name)
+ */
 class Role extends Model
 {
+    /** @var string ROLE_PATIENT */
+    public const ROLE_PATIENT   = 'patient';
+
+    /** @var string ROLE_DOCTOR */
+    public const ROLE_DOCTOR    = 'doctor';
+
+    /** @var string ROLE_ADMIN */
+    public const ROLE_ADMIN     = 'admin';
+
+    /** @var string[] ROLES */
+    public const ROLES = [
+        self::ROLE_PATIENT,
+        self::ROLE_DOCTOR,
+        self::ROLE_ADMIN,
+    ];
+
     use HasFactory;
+
+    public $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -15,5 +43,13 @@ class Role extends Model
     public function users(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    /**
+     * @param string $roleName
+     * @return int
+     */
+    public static function getRoleIdByName(string $roleName): int {
+        return self::whereName($roleName)->pluck('id')->first();
     }
 }
---
+++ database/migrations/2022_01_27_181037_add_basic_roles.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBasicRoles extends Migration
+{
+
+    /** @var array $roles */
+    private array $roles = [
+        Role::ROLE_PATIENT,
+        Role::ROLE_DOCTOR,
+        Role::ROLE_ADMIN,
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->roles as $roleName) {
+            $role = new Role();
+            $role->name = $roleName;
+            $role->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Role::whereIn('name', [$this->roles])
+            ->delete();
+    }
+}
