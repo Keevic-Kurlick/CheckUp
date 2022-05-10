ce455620aa02502f5f9683f28cfa76f6c841e7da
Weipy <nikita.savelev1999@mail.ru>
2022-05-10 15:24:29 +0300

    profile: orders: show  order was styled.

--- resources/views/layouts/profile/orders/show.blade.php
+++ resources/views/layouts/profile/orders/show.blade.php
@@ -13,7 +13,7 @@
 @section('content')
     <div class="container order_info">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-8 col-lg-6">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
