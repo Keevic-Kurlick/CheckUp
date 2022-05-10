3fd99550fae34842cc05f13cc89a00072334bb43
Weipy <nikita.savelev1999@mail.ru>
2022-05-10 15:14:39 +0300

    profile: orders: added display of doctor's message about cancellation.

--- resources/views/layouts/profile/orders/show.blade.php
+++ resources/views/layouts/profile/orders/show.blade.php
@@ -3,7 +3,8 @@
      * @var \App\Models\Order $order
      */
 
-    $hasApproveMessage = !empty($order->orderResult?->approve_message);
+    $hasApproveMessage  = !empty($order->orderResult?->approve_message);
+    $hasCancelMessage   = !empty($order->orderResult?->cancel_message);
     $hasCertificatePath = !empty($order->orderResult?->certificate_path);
 @endphp
 
@@ -63,6 +64,20 @@
                                             Обратитесь в техническую поддержку.
                                         </p>
                                     @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @if($order->status == \App\Models\Order::CANCEL_STATUS)
+                    <div class="row mt-2">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <span>Причина отказа</span>
+                                </div>
+                                <div class="card-body">
+                                    {{ $order->orderResult->cancel_message }}
                                 </div>
                             </div>
                         </div>
