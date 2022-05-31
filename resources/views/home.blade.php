5ebd4ce30f596bcc017d247597a8447fc163a56c
Weipy <nikita.savelev1999@mail.ru>
2022-05-31 22:23:58 +0300

    welcome text was changed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <div class="card">
                 <div class="card-header" style="text-align: center;">{{ __('Добро пожаловать!') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                @if(Auth::user()->role->name === \App\Models\Role::ROLE_PATIENT)
 
-                    {{ __('Вы авторизованы! Теперь вы можете заказывать справки, отслеживать статусы заказов, добавлять и изменять документы в личном кабинете.') }}
-                </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('Вы авторизованы! Теперь вы можете заказывать справки, отслеживать статусы заказов, добавлять и изменять документы в личном кабинете.') }}
+                    </div>
+                @endif
             </div>
         </div>
     </div>
