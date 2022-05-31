4feef675ee749697e31997e464163755f2d2edc5
Weipy <nikita.savelev1999@mail.ru>
2022-06-01 00:25:47 +0300

    unnecessary uses have been removed.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\Role;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
